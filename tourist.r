a9f3358fa50814ce9e746e8cb977148c593e6b89
Àlex Fiestas <afiestas@kde.org>
2016-08-10 12:30:12 +0200

    Sanitize more data in neighborhood area, population...
    - Numbers have dots for the thousands
    - Neighborhoods include not populated areas
    - Neighborhoods have different names

--- tourist.r
+++ tourist.r
@@ -57,15 +57,36 @@ l = ldply(dades, extractHotel)
 rm(dades)
 
 hoods = read.csv2('superficiedens2013.csv', stringsAsFactors = F, encoding = 'UTF-8', fileEncoding = 'ISO8859-1')
-hoods$Barris <-  gsub('\\d+\\. ', '', hoods$Barris, perl=T)
-hoods <- hoods[hoods$Barris != "", ]
-
-#Lets check if we are missing any neighborhoods in our neighborhood df
-hoods[hoods$Barris %nin% neighborhoods$NAME , ]
-
-#We are missing 8 neighborhoods wich do not have touristic houses
-#1 of them just have different names
-hoods[hoods$Barris == 'el Poble Sec - AEI Parc Montjuïc (1)', ] <- 'el Poble Sec'
 
 #Translate collumn names from Catalan to English
-colnames(hoods) <- c('Dte', 'NAME', 'POPULATION', 'AREA', 'DENSITY', 'DENSITAT_NETA')
+colnames(hoods) <- c('Dte', 'name', 'population', 'area', 'density', 'net.density')
+
+#remove rows without names
+hoods <- hoods[hoods$name != "", ]
+
+#CSV has dot in the thousands
+hoods$population <- as.numeric(gsub('\\.', '', hoods$population))
+hoods$area <- as.numeric(gsub('\\.', '', hoods$area))
+hoods$density <- as.numeric(gsub('\\.', '', hoods$density))
+hoods$net.density <- as.numeric(gsub('\\.', '', hoods$net.density))
+
+#Remove enumerator in the name 23. name --> name
+hoods$name <-  gsub('\\d+\\. ', '', hoods$name, perl=T)
+
+#Lets check if we are missing any neighborhoods in our neighborhood df
+hoods[hoods$name %nin% neighborhoods$name , ]
+
+#We are missing 7 neighborhoods wich do not have touristic houses
+# and 2 whos name include not populated areas such insdustrial parks
+hoods[hoods$name == 'el Poble Sec - AEI Parc Montjuïc (1)', ]$name <- 'el Poble Sec'
+hoods[hoods$name == 'la Marina del Prat Vermell - AEI Zona Franca (2)', ]$name <- 'la Marina del Prat Vermell'
+
+#Removed not populated area in two neighborhoods according to source
+#http://www.bcn.cat/estadistica/catala/dades/name/terri/sup/sup415.htm (AEI)
+hoods[hoods$name == 'el Poble Sec', ]$area <- hoods[hoods$name == 'el Poble Sec', ]$area - 366.1
+hoods[hoods$name == 'la Marina del Prat Vermell', ]$area <- hoods[hoods$name == 'la Marina del Prat Vermell', ]$area - 1353.1
+
+neighborhoods <- merge(neighborhoods, hoods[, -1])
+
+#We are done with this df
+rm(hoods)
