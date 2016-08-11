bef7a7c202ca5eddeaa6d8b501620e15ca764a22
Àlex Fiestas <afiestas@kde.org>
2016-08-11 02:43:03 +0200

    Ask for a street number
    Not all streets starts with number 1 so we need to ask for a number to
    the api

--- getStreetInfo.R
+++ getStreetInfo.R
@@ -10,7 +10,24 @@ while(length(line <- readLines(f,n=1)) > 0) {
   streetInfo = strsplit(line, ':')
   streetName = streetInfo[[1]][1]
   streetNum = streetInfo[[1]][2]
-  # message(paste(streetName, streetNum))
+  # # write(paste(streetName, streetNum), stderr())
+  # 
+  # r = POST('http://w20.bcn.cat/GuiaWS/GuiaWS.asmx/LocalizaVia', body = list(Via = streetName), encode = 'form')
+  # xml = content(r, as='text')
+  # root = xmlRoot(xmlParse(xml))
+  # if (is.null(root[['rDatosVia']][['NumTrobat']])) {
+  #   write(paste("Couldn't find street", streetName), stderr())
+  #   next()
+  # }
+  # 
+  # num = xmlApply(root[['rDatosVia']][['NumTrobat']], xmlValue)
+  # # write(paste("Found number:", num), stderr())
+  # 
+  # if (length(num) == 0) {
+  #   write(paste("Couldn't fetch number", streetName), stderr())
+  #   next()
+  #   write(paste("11111111111111111111111111", streetName), stderr())
+  # }
 
   r = POST('http://w20.bcn.cat/GuiaWS/GuiaWS.asmx/DireccionPostal', body = list(Calle = streetName, Numero = streetNum), encode = 'form')
   if ( r$status_code != 200 ) {
@@ -19,6 +36,11 @@ while(length(line <- readLines(f,n=1)) > 0) {
   }
   xml = content(r, as='text')
   root = xmlRoot(xmlParse(xml))
+  if (is.null(root[['Direcciones']][['Direccion']])) {
+    write(paste0("WTF: ", streetName," ",streetNum), stderr())
+    next()
+  }
+
   names = xmlApply(root[['Direcciones']][['Direccion']], xmlName)
   values = xmlApply(root[['Direcciones']][['Direccion']], xmlValue)
   
